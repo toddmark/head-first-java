f4b254e75e187c15acbc71c9eb296eb13862b8dc
toddmark <toddmark0315@gmail.com>
2019-07-05 16:54:02 +0800

    init

---
+++ Dog.java
@@ -0,0 +1,14 @@
+class Dog {
+  int size;
+  String breed;
+  String name;
+  vodi bark() {
+    System.out.println("Ruff!");
+  }
+}
+
+class DogTestDrive {
+  public static void main(String[] args) {
+    // Test Dog
+  }
+}
---
+++ Hello.java
@@ -0,0 +1,9 @@
+/**
+ * Hello
+ */
+public class Hello {
+  public static void main(String[] args) {
+    String x = "todd mar";
+    System.out.println(x);
+  }
+}
